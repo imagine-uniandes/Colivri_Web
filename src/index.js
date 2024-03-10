98c376d0fed461e75b014b12c451ac38a20c29d1
Abel Arismendy <abel@arismendy.co>
2024-03-10 14:04:12 -0500

    upgrade root to react 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import {createRoot} from "react-dom/client";
 import App from './components/App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,10 +7,13 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './styles/index.css';
 
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("No root element found");
+}
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+createRoot(rootElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
