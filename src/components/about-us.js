37aed16e58bae3d68c3817e63889f89a810bb3c8
Abel Arismendy <abel@arismendy.co>
2024-04-08 20:46:28 -0500

    txt

--- src/components/about-us.js
+++ src/components/about-us.js
@@ -1,6 +1,7 @@
 import React from "react";
 import '../styles/about.css';
 import logo from "../assets/imgs/logo.png";
+import iso from "../assets/imgs/colivri-iso.png";
 
 function Navbar() {
     return (
@@ -13,21 +14,11 @@ function Navbar() {
                         <img src={logo} alt="logo"></img>
                     </div>
                     <div className="col-sm-12 col-lg-6 info">
-                        <li>
-                            <ul>
-                                <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) de</p>
-                            </ul>
-
-                            <ul>
-                                <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) de</p>
-                            </ul>
-
-                            <ul>
-                                <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) de</p>
-                            </ul>
-                        </li>
+                        <p>Colivri (Colaboratorio en Interacción, Visualización, Robótica y Sistemas Autónomos) de la Universidad de los Andes es un espacio de colaboración entre diversos departamentos de la Universidad como Ingeniería de Sistemas, Ingeniería Mecánica, Ingeniería Industrial, Ingeniería Eléctrica y Electrónica, entre otras.</p>
+                        <p>En Colivri nos dedicamos a la investigación y desarrollo de tecnologías en varias disciplinas. Trabajamos en el desarrollo de experiencias computacionales con interfaces novedosas. La Realidad Virtual y Aumentada, los simuladores, y las interfaces robot humano son algunas de las tecnologías que usamos para el desarrollo de nuevas soluciones a problemas locales e internacionales.</p>
                     </div>
                 </div>
+
             </div>
 
             
